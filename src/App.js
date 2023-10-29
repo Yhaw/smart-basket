bb359683d43cf2e9b13d60339d63379514a2a608
Sylvian <yhawkhisses2@gmail.com>
2023-10-29 14:03:30 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import UserContext from './pages/UserContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/login/login';
+import Scan from './pages/scan/scan';
+import Cart from './pages/cart/cart';
+import { ProjectProvider } from './ProjectContext';
+import AdminPage from './pages/Admin/admin';
+import CheckoutPage from './pages/checkout/checkout';
 
 function App() {
+  const [userId, setUserId] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <UserContext.Provider value={{ userId, setUserId }}>
+        <ProjectProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/scan" element={<Scan />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/checkout" element={<CheckoutPage/>} />
+
+
+            {/* <Route path="/propage" element={<Propage />} />
+            <Route path="/field" element={<Field/>} />
+            <Route path="/training" element={<Training/>} />
+            <Route exact path="/projects/:projectId" element={<Project/>} /> */}
+
+
+          </Routes>
+        </ProjectProvider>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
